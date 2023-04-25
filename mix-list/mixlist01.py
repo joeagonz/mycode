69e8dea48a486f5e1c012acffb5ebc9de6496c80
joeagonz <joseph.gonzalez.14@us.af.mil>
2023-04-25 17:45:48 +0000

    iplist CHALLENGE

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+# displays
+
+# print("The first item in the list (IP): " + my_list[0])
+
+# displays
+
+# print("The second item in the list (port): " + str(my_list[1]))
+
+# displays
+
+# print("The last item in the list (state): " + my_list[2])
+
+# displays
+
+print("The IP addresses are:", iplist[3] + " and " + iplist[4])
+
