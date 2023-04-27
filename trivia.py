40cd9305be740c21d4c97da8ff4e6fb7232fdd6d
joeagonz <joseph.gonzalez.14@us.af.mil>
2023-04-27 16:43:07 +0000

    studying for logic

---
+++ trivia.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import html
+
+trivia= {
+
+        "category": "Entertainment: Film",
+        "type": "multiple",
+        "question": "Which of the following is NOT a quote from the 1942 film Casablanca? ",
+        "correct_answer": "&quot;Frankly, my dear, I don&#039;t give a damn.&quot;",
+        "incorrect_answers": [
+            "&quot;Here&#039;s lookin&#039; at you, kid.&quot;",
+            "&ldquo;Of all the gin joints, in all the towns, in all the world, she walks into mine&hellip;&rdquo;",
+            "&quot;Round up the usual suspects.&quot;"
+
+            ]
+        }
+
+question= trivia.get("question")
+correct= html.unescape(trivia.get("correct_answer"))
+incorrect1= html.unescape(trivia.get("incorrect_answers")[0])
+incorrect2= html.unescape(trivia.get("incorrect_answers")[1])
+incorrect3= html.unescape(trivia.get("incorrect_answers")[2])
+
+print(f"Question: { question }")
+print()
+print(f"Correct Answer: { correct }")
+print(f"Incorrect Answer: { incorrect1 }")
+print(f"Incorrect Answer: { incorrect2 }")
+print(f"Incorrect Answer: { incorrect3 }")
+
+
